da068fc08a7d7d4c6ea9d0480c1f563e95944b4e
ShotaKiko <Shota.Kikozashvili@gmail.com>
2019-01-26 13:58:38 -0600

    Created subclass CubeMaker extending from cuboidMakerRefactor/Completed classes.js stretch goal of subclass's volume and SA logging

--- challenges/classes.js
+++ challenges/classes.js
@@ -36,8 +36,28 @@ class CuboidMakerRefactor {
 const cuboidRefactor = new CuboidMakerRefactor(4, 5, 5)
 
 // Test your volume and surfaceArea methods by uncommenting the logs below:
-console.log('~~~~~~~~~~~~~~~~~~Below is the refactored to class syntax version~~~~~~~~~~~~~~~~~~~~~~~~~~~~``')
+console.log('~~~~~~~~~~~~~~~~~~Below is the refactored to class syntax version~~~~~~~~~~~~~~~~~~~~~~~~~~~~')
 console.log(cuboidRefactor.calculateVolume()); // 100
 console.log(cuboidRefactor.calculateSurfaceArea()); // 130
 
-// Stretch Task: Extend the base class CuboidMaker with a sub class called CubeMaker.  Find out the formulas for volume and surface area for cubes and create those methods using the dimension properties from CuboidMaker.  Test your work by logging out your volume and surface area.
+// Stretch Task: Extend the base class CuboidMaker with a sub class called CubeMaker.  
+//Find out the formulas for volume and surface area for cubes and create those methods using the dimension properties from CuboidMaker.  
+//Test your work by logging out your volume and surface area.
+
+class CubeMaker extends CuboidMakerRefactor {
+    constructor(length, width, height){
+        super(length, width, height);
+    }
+    cubeVolume(){
+        return `The volume of this cube is ${this.length**3} units cubed.`
+    }
+    cubeSA(){
+        return `The surface area of this cube is ${6 * (this.length** 2)} units squared.`
+    }
+}
+
+const gameCube = new CubeMaker(2, 2, 2)
+console.log('')
+console.log('~~~~~~~~~~~~~~~~~~Below is the stretch goal logs for cube volume and SA~~~~~~~~~~~~~~~~~~~~~~~~~~~~``')
+console.log(gameCube.cubeVolume()); // 8
+console.log(gameCube.cubeSA()); //24
